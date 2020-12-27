f58d319a2f8a8977a6ba2896cc3a5c8d22eefad3
megumi.kimura <megu.megu.megu68@gmail.com>
2020-12-27 14:35:40 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create,:edit,:update]
