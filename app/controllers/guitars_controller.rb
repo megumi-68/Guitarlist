e2c810ee9b1a6ec69a9d87213ec2f110b23ceeee
megumi.kimura <megu.megu.megu68@gmail.com>
2020-12-30 15:07:56 +0000

    delete

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -1,17 +1,42 @@
 class GuitarsController < ApplicationController
+    before_action :require_user_logged_in
     
+    def index
+        @guitars = Guitar.all
+    end
     
     def new
         @guitar = Guitar.new
     end
- 
-    def create
-        Guitar.create(guitar_params)
+  
+  def create
+    @guitar = current_user.guitars.build(guitar_params)
+    if @guitar.save
+      flash[:success] = '投稿しました。'
+      redirect_to root_url
+    else
+      @guitars = current_user.guitars.order(id: :desc).page(params[:page])
+      flash.now[:danger] = '投稿に失敗しました。'
+      render 'toppages/index'
     end
+  end
 
-    private
-    
-    def guitar_params
-        params.require(:guitar).permit(:name, :category, :content)
+  def destroy
+    @guitar.destroy
+    flash[:success] = '削除しました。'
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def guitar_params
+    params.require(:guitar).permit(:content,:name,:category)
+  end
+
+  def correct_user
+    @guitar = current_user.guitars.find_by(id: params[:id])
+    unless @guitar
+      redirect_to root_url
     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'guitars/new', to: 'guitars#new'
   post 'guitars', to: 'guitars#create'
-  resources :guitars, only: [:create, :destroy,:edit,:update]
+  resources :guitars, only: [:create, :destroy, :edit, :update]
 end
